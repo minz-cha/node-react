54aa3dcc5b8a75988b36b3c994c082b052903f61
minju kim <demin1026@kpu.ac.kr>
2023-06-12 03:31:11 +0900

    회원가입 기능 간단 구현

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 const mongoose = require('mongoose')
-var mongodb_pwd = require('./secret.json')
+const mongodb_pwd = require('./secret.json')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+//application/json
+app.use(bodyParser.json())
 
 const url = `mongodb://ming:${mongodb_pwd.mongodb_pwd}@ac-tnjmlt2-shard-00-00.10bqcpc.mongodb.net:27017,ac-tnjmlt2-shard-00-01.10bqcpc.mongodb.net:27017,ac-tnjmlt2-shard-00-02.10bqcpc.mongodb.net:27017/?ssl=true&replicaSet=atlas-iuuepm-shard-0&authSource=admin&retryWrites=true&w=majority`
 
@@ -16,5 +23,23 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     });
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/register', (req, res) => {
+    // 회원가입할 때 필요한 정보들을 client에서 받고
+    // 해당 값을 DB에 넣음
+
+    const user = new User(req.body)
+
+    user.save()
+        .then((userInfo) => {
+            return res.status(200).json({
+                success: true
+            });
+        })
+        .catch((err) => {
+            return res.json({ success: false, err });
+        });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
