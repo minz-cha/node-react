2a31484940e3848d3d9c940e54963973524c0dd2
minju kim <demin1026@kpu.ac.kr>
2023-06-12 23:59:25 +0900

    환경변수 설정

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+
+const config = require('./config/key')
 const { User } = require('./models/User')
 const mongoose = require('mongoose')
-const mongodb_pwd = require('./secret.json')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 //application/json
 app.use(bodyParser.json())
 
-const url = `mongodb://ming:${mongodb_pwd.mongodb_pwd}@ac-tnjmlt2-shard-00-00.10bqcpc.mongodb.net:27017,ac-tnjmlt2-shard-00-01.10bqcpc.mongodb.net:27017,ac-tnjmlt2-shard-00-02.10bqcpc.mongodb.net:27017/?ssl=true&replicaSet=atlas-iuuepm-shard-0&authSource=admin&retryWrites=true&w=majority`
+const url = config.mongoURI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log('MongoDB에 연결되었습니다.');
+        console.log('MongoDB에 연결되었습니다.')
         // 여기에 추가적인 작업이나 애플리케이션 실행 코드를 작성할 수 있음
     })
     .catch((error) => {
-        console.error('MongoDB 연결에 실패했습니다.', error);
-    });
+        console.error('MongoDB 연결에 실패했습니다.', error)
+    })
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -38,8 +39,8 @@ app.post('/register', (req, res) => {
             });
         })
         .catch((err) => {
-            return res.json({ success: false, err });
-        });
+            return res.json({ success: false, err })
+        })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
