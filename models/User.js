dee8656c6087543c017c2de5fd98d35458b2e787
minju kim <demin1026@kpu.ac.kr>
2023-06-24 15:49:52 +0900

    bcrypt로 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -25,6 +28,22 @@ const userSchema = mongoose.Schema({
     tokenExp: {  //토큰 유효기간
         type: Number
     }
+})
+
+// 'save'동작 전에 이뤄짐
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    // 비밀번호 암호화 - Salt 이용
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        // 에러 경우 - save의 err 처리로 넘겨줌
+        if (err) return next(err)
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
 })
 
 const User = mongoose.model('User', userSchema)
