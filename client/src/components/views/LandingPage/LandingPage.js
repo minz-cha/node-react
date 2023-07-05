2437a6001b007f6942ed3098a2d4a5f1452f76e8
minju kim <demin1026@kpu.ac.kr>
2023-07-05 11:47:33 +0900

    login input form 생성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function LandingPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const onPasswordHandler = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        console.log('Email', Email)
+        console.log('Password', Password)
+    };
+
 
     useEffect(() => {
         axios.get('/api/hello')
@@ -9,8 +28,23 @@ function LandingPage() {
     }, [])
 
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type='email' value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type='password' value={Password} onChange={onPasswordHandler} />
+
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
